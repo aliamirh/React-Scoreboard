096eb7f6fa20462ed93b3e14ee5f4dc2eb6b6ea2
Ali Amirhamzeh <aliamirhamzeh@gmail.com>
2020-02-10 13:15:41 -0800

    create scoreboard table

--- app.js
+++ app.js
@@ -1,16 +1,44 @@
-const desc   = 'I just learned how to create a React node and render it into the DOM.';
-const myTitleID = 'main-title';
-const name = 'Ali';
-const lastName = 'Amirhamzeh'
+const Header = () => {
+    return (
+        <header>
+            <h1>Scoreboard</h1>
+            <span className="stats">Players: 1</span>
+        </header>
+    );
+}
 
-const header = (
-    <header>
-    <h1 id={myTitleID}>{name} {lastName} 's First React Element</h1>
-    <p>{desc}</p>
-    </header>
-);
+const Player = () => {
+    return (
+        <div className ="player">
+            <span className ="player-name">
+            Ali
+            </span>
+            <Counter />
+        </div>
+    );
+}
+
+const Counter = () => {
+    return (
+        <div className="counter">
+            <button className="counter-action decrement"> - </button>
+            <span className="counter-score">35</span>
+            <button className="counter-action increment"> + </button>
+        </div>
+    );
+}
+
+const App = () => {
+    return (
+        <div className="scoreboard">
+            <Header />
+            {/* Players list */}
+            <Player />
+        </div>
+    );
+}
 
 ReactDOM.render(
-    header,
+    <App />,
     document.getElementById('root')
 );
